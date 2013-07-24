6213e65c1b90ab15acfc128459fea15bcd908fa0
gongpengjun <frank.gongpengjun@gmail.com>
2013-07-24 21:18:21 +0800

    initial commit

---
+++ NoLimitGuess/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  NoLimitGuess
+//
+//  Created by 巩 鹏军 on 7/24/13.
+//  Copyright Gong Pengjun 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ NoLimitGuess/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NoLimitGuess
+//
+//  Created by 巩 鹏军 on 7/24/13.
+//  Copyright Gong Pengjun 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
